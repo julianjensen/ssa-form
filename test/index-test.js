97ffddd82076bf06870ef515c2c78f56f176ad25
Julian Jensen <jjdanois@gmail.com>
2018-01-18 08:59:04 -0500

    Added placeholder for unit tests

---
+++ test/index-test.js
@@ -0,0 +1,17 @@
+/** ******************************************************************************************************************
+ * @file Unit test placeholder.
+ * @author Julian Jensen <jjdanois@gmail.com>
+ * @since 1.0.0
+ * @date 18-Jan-2018
+ *********************************************************************************************************************/
+"use strict";
+
+const
+    expect = require( 'chai' ).expect;
+
+describe( 'ssa-form', function() {
+
+    it( 'should have actual tests', () => {
+        expect( true ).to.be.true;
+    } );
+} );
